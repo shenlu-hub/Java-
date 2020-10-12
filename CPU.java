67c4b1abc7b7ad284ff022d57ebf9da2834b39cc
shenlu-hub <72739896+shenlu-hub@users.noreply.github.com>
2020-10-12 20:10:11 +0800

    Update CPU.java

--- CPU.java
+++ CPU.java
@@ -2,11 +2,19 @@ package sec;
 
 public class CPU {
 	private int speed;
+	public int price;
 	int getSpeed(){
 		return speed;
 	}
 	public void setSpeed(int speed){
 		this.speed = speed;
+	}
+	
+	int getPrice(){
+		return price;
+	}
+	public void setPrice(int price){
+		this.price = price;
 	}
 	CPU(){
 		
