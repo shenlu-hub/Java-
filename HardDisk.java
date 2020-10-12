cb9ff69e3f7daf0e53cc5292cc04456f2f55cfa8
shenlu-hub <72739896+shenlu-hub@users.noreply.github.com>
2020-10-12 20:11:31 +0800

    Update HardDisk.java

--- HardDisk.java
+++ HardDisk.java
@@ -2,12 +2,22 @@ package sec;
 
 public class HardDisk {
 	private int amout;
+	public int size;
 	int getAmount(){
 		return amout;
 	}
-	public void setAmount(int amout){
+    public void setAmount(int amout){
 		this.amout = amout;
 	}
+    
+    
+    int getSize(){
+		return size;
+	}
+	public void setSize(int size){
+		this.size = size;
+	}
+	
 	HardDisk(){
 		
 	}
